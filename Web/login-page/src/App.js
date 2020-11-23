350cb3b11620a024b0662db3b80a55181773c046
NgocHieuDev <hoccungbillnguyen@gmail.com>
2020-11-23 23:29:45 +0700

    config 1

--- Web/login-page/src/App.js
+++ Web/login-page/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import LoginPage from './ui/page/login-page';
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <LoginPage />
+    </>
   );
 }
 
---
+++ Web/login-page/src/ui/components/login-comp/SideBarComp/sidebar-comp.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './sidebar-comp.scss';
+const SideBarComp = () => {
+    return (
+        <>
+            <nav id="side-bar">
+                <ul className="menu-sidebar">
+                    <li>Item 1</li>
+                    <li>Item 2</li>
+                    <li>Item 3</li>
+                    <li>Item 4</li>
+                </ul>
+            </nav>
+        </>
+    )
+}
+
+export default SideBarComp
